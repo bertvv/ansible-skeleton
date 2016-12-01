ac6314ddda78920c4ecc3ad4a0eaf904cc2f3e4a
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2016-12-01 16:05:56 +0100

    Improve the Ansible installation process, now works on both Fedora and CentOS

--- scripts/playbook-win.sh
+++ scripts/playbook-win.sh
@@ -11,31 +11,72 @@
 # @version 1.0
 #
 
-playbook=/vagrant/ansible/site.yml
-inventory=/vagrant/scripts/inventory.py
+#
+# Bash shell settings: exit on failing commands, unbound variables
+#
+set -o errexit
+set -o nounset
+set -o pipefail
+
+#
+# Variables
+#
+readonly playbook=/vagrant/ansible/site.yml
+readonly inventory=/vagrant/scripts/inventory.py
+
+#
+# Functions
+#
+
+# Print the Linux distribution
+get_linux_distribution() {
+  if user_exists vyos; then
+    echo "vyos"
+  elif [ -f /etc/redhat-release ]; then
+    cut -f1 -d' ' /etc/redhat-release | tr "[:upper:]" "[:lower:]"
+  fi
+}
+
+# Install Ansible on a Fedora system.
+# The version in the repos is fairly current, so we'll install that
+install_ansible_fedora() {
+  echo "Installing Fedora from repositories"
+  dnf -y install ansible
+}
+
+# Install Ansible on a CentOS system from EPEL
+install_ansible_centos() {
+  yum -y install epel-releas
+  yun -y install ansible
+}
+
+# Checks if the specified user exists
+user_exists() {
+  user_name="${1}"
+  getent passwd "${user_name}" > /dev/null
+}
+#
+# Script proper
+#
+
+# If we're on a VyOS box, this script shouldn't be executed
+if user_exists vyos; then
+  exit 0
+fi
 
 if [ ! -f ${playbook} ]; then
-  echo "Cannot find Ansible playbook."
+  echo "Cannot find Ansible playbook ${playbook}."
   exit 1
 fi
 
 if [ ! -f ${inventory} ]; then
-  echo "Cannot find inventory file."
+  echo "Cannot find inventory file ${inventory}."
   exit 2
 fi
 
 # Install Ansible and its dependencies if it's not installed already.
 if [ ! -f /usr/bin/ansible ]; then
-  echo "Installing Ansible dependencies and build tools."
-  yum install -y gcc git python-devel libffi-devel openssl-devel python-setuptools
-  echo "Installing pip via easy_install."
-  easy_install pip
-  # Make sure setuptools are installed crrectly.
-  pip install setuptools --no-binary :all: --upgrade
-  echo "Installing Ansible."
-  pip install ansible
-  echo "Ansible installed:"
-  ansible --version
+  "install_ansible_$(get_linux_distribution)"
 fi
 
 ansible-playbook "${playbook}" \
